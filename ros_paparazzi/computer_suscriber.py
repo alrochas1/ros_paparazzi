9cc2958b89bef5d287726891ca30c2ed639f3e27
alrochas1 <alrochas@ucm.es>
2024-11-06 15:50:22 +0100

    Added txt reading for the data

--- ros_paparazzi/computer_suscriber.py
+++ ros_paparazzi/computer_suscriber.py
@@ -16,15 +16,12 @@ class Computer_Subscriber(Node):
         self.create_timer(3, self.datalink_callback)
         
     def telemetry_callback(self, msg):
-        self.get_logger().info('Receiving data: [{msg.longitude}, {msg.latitude}, {msg.altitude}]')
+        self.get_logger().info(f'Receiving data: [{msg.longitude}, {msg.latitude}, {msg.altitude}]')
 
     def datalink_callback(self):
 
         # Temporal
-        lat = int(40.4506399 * 1e7 + (random.randint(-10000, 10000)))
-        lon = int(-3.7260463 * 1e7 + (random.randint(-10000, 10000)))
-        alt = int(650.0 * 1e3)
-        wp_id = int(14)
+        [lat, lon, alt, wp_id] = self.get_data()
 
         msg = Waypoint()
         # msg.header.stamp.sec = int(autopilot_data.tiempo)
@@ -35,6 +32,29 @@ class Computer_Subscriber(Node):
         msg.wp_id = wp_id
         self.publisher.publish(msg)
         self.get_logger().info(f'Publishing data: [{lat * 1e-7:.7f}, {lon * 1e-7:.7f}]')
+
+
+    # TEMPORAL: To simulate the data
+    def get_data(self):
+        try:
+            with open("~/data.txt", "r") as file:
+                lines = file.readlines()
+                
+                # For moving randomly
+                latitude = int(40.4506399 * 1e7 + (random.randint(-10000, 10000)))
+                longitude = int(-3.7260463 * 1e7 + (random.randint(-10000, 10000)))
+
+                # For using the txt
+                latitude = int(self.get_value_from_line(lines, "latitude"))
+                longitude = int(self.get_value_from_line(lines, "longitude"))
+                altitude = int(self.get_value_from_line(lines, "altitude"))
+                wp_id = int(self.get_value_from_line(lines, "waypoint_id"))
+                
+                return latitude, longitude, altitude, wp_id
+            
+        except Exception as e:
+            self.get_logger().error(f"Error al leer el archivo: {e}")
+            return 40.4506399, -3.7260463, 650.0, 14  # Valores por defecto en caso de error
 
 
 
